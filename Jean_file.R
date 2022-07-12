c3651765beb3594510ded0dc976eacb5755e0e9d
Jennifer Osterhage <105387058+jenniferosterhage@users.noreply.github.com>
2022-07-12 11:20:40 -0400

    Update Jean_file.R

--- Jean_file.R
+++ Jean_file.R
@@ -9,7 +9,7 @@
 add_numbers <- function(number_1, number_2) {
   number_1 + number_2
 }
-add_numbers(5, 10)
+add_numbers(5, 6)
 #this function takes a csv and turning it into a dataframe (the Rformat for the data) - an R data structure. read.csv reads csv data
 sci_data<-read.csv("data/sci-online-classes.csv")
 
